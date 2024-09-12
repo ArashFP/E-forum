10d83a34b3ab8c5d94a142153501fc4e1f7eaff1
Arash Falah Parsa <arash_afp_94@hotmail.com>
2024-09-12 14:50:09 +0200

    adds login and sign to navbar

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -2,17 +2,25 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>
-      <ul className="flex justify-center items-center text-teal-400 text-lg font-serif py-8 px-4 gap-x-3 bg-slate-200 font-bold">
+    <div className= "flex justify-between bg-slate-200">
+      <ul className="flex justify-center items-center text-teal-700 text-lg font-serif py-8 px-4 gap-x-3 bg-slate-200 font-bold">
         <Link href="/" >
           Home 
         </Link>
         <Link href="/create" >
           Create 
         </Link>
-        {/* <Link href="/threads" >
-          Threads
-        </Link> */}
+      </ul>
+      <h1 className= "flex items-center text-teal-700 text-6xl font-bold">
+         Tech Hub
+      </h1>
+      <ul className="flex justify-center items-center text-teal-700 text-lg font-serif py-8 px-4 gap-x-3 bg-slate-200 font-bold">
+        <Link href="sign-up">
+          Sign-Up
+        </Link>
+        <Link href="sign-in">
+          Login
+        </Link>
       </ul>
     </div>
   )
